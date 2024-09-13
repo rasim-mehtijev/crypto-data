aea226d2022c7b77a8a0d97d9582934d241d7187
Rasim <rasim.mehtijev@insly.com>
2024-09-13 20:57:20 +0300

    Converter, fontawesome. Buggy

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import ListCoins from "./ListCoins";
 import CoinPage from "./CoinPage";
 import GlobalData from "./GlobalData";
 import SearchResult from "./SearchResult";
+import Converter from "./Converter";
 
 function App() {
   return (
     <Container>
       <Navigation />
+      <Converter />
       <GlobalData />
       {/* <ListCoins /> */}
       {/* <CoinPage /> */}
---
+++ src/Converter.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowsRotate } from "@fortawesome/free-solid-svg-icons";
+
+const initialState = {
+  from: 100,
+  to: 500,
+};
+
+function Converter() {
+  const [values, setValues] = React.useState(initialState);
+  const [leftToRight, setLeftToRight] = React.useState(true);
+
+  const handleClick = () => {
+    setValues((prevValues) => ({
+        from: !leftToRight ? prevValues.from : prevValues.to,
+        to: !leftToRight ? prevValues.to : prevValues.from,
+      }));
+    
+      setLeftToRight((prevLeftToRight) => !prevLeftToRight);
+  };
+
+  return (
+    <Row className="g-2">
+      <Col md>
+        <InputGroup>
+          <FloatingLabel controlId="fromInput" label="From">
+            <Form.Control
+              type="text"
+              placeholder="0"
+              value={values.from}
+              defaultValue={values.from}
+            />
+          </FloatingLabel>
+          <FloatingLabel controlId="from" label="Coin">
+            <Form.Select>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </Form.Select>
+          </FloatingLabel>
+        </InputGroup>
+      </Col>
+      <Col>
+        <FontAwesomeIcon icon={faArrowsRotate} onClick={handleClick} />
+      </Col>
+      <Col md>
+        <InputGroup>
+          <FloatingLabel controlId="toInput" label="To">
+            <Form.Control
+              type="text"
+              placeholder="0"
+              value={values.to}
+              defaultValue={values.to}
+            />
+          </FloatingLabel>
+          <FloatingLabel controlId="to" label="Coin">
+            <Form.Select>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </Form.Select>
+          </FloatingLabel>
+        </InputGroup>
+      </Col>
+    </Row>
+  );
+}
+
+export default Converter;
