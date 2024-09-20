0031e154808533b3949dc93e71b82b23308f9f6c
Rasim <rasim.mehtijev@insly.com>
2024-09-20 18:56:39 +0300

    return listCoins

--- src/Body/Body.jsx
+++ src/Body/Body.jsx
@@ -1,13 +1,11 @@
 import GlobalData from "./GlobalData";
 import ListCoins from "./ListCoins";
-import CoinPage from './CoinPage';
 
 function Body() {
   return (
     <>
       <GlobalData />
-      {/* <ListCoins /> */}
-      <CoinPage />
+      <ListCoins />
     </>
   );
 }
