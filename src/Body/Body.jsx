faaea7a6f2f17585fe775408b1794264ba9a9448
Rasim Mehtijev <123364630+rasim-mehtijev@users.noreply.github.com>
2024-09-24 21:09:14 +0300

    coin price data, PriceNumber, currency select (#9)
    * coin price data, PriceNumber, currency select
    
    * remove console.log

--- src/Body/Body.jsx
+++ src/Body/Body.jsx
@@ -1,11 +1,11 @@
 import GlobalData from "./GlobalData";
 import ListCoins from "./ListCoins";
 
-function Body() {
+function Body(props) {
   return (
     <>
       <GlobalData />
-      <ListCoins />
+      <ListCoins {...props} />
     </>
   );
 }
---
+++ src/Body/PriceNumber.jsx
@@ -0,0 +1,9 @@
+import { NumericFormat } from "react-number-format";
+
+const PriceNumber = ({ value }) => {
+  return (
+    <NumericFormat value={value} thousandSeparator=" " displayType="text" prefix="$"/>
+  );
+};
+
+export default PriceNumber;
