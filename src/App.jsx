ecb4a4e2337982b7477abe0d57a39df54acd654b
Rasim <rasim.mehtijev@insly.com>
2024-09-24 21:06:22 +0300

    coin price data, PriceNumber, currency select

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import './App.scss';
+import React from "react";
+import "./App.scss";
 import Container from "react-bootstrap/Container";
 import Header from "./Header";
 import Body from "./Body";
+import { currencies } from "./constants";
 
 function App() {
+  const [selectedCurrency, setSelectedCurrency] = React.useState(currencies[0]);
+  console.log('selectedCurrency', selectedCurrency);
   return (
     <Container>
-      <Header />
-      <Body />
+      <Header
+        selectedCurrency={selectedCurrency}
+        setSelectedCurrency={setSelectedCurrency}
+      />
+      <Body selectedCurrency={selectedCurrency} />
     </Container>
   );
 }
