49cde828f762ac564bcf4f5cc12944090398f0b7
Rasim Mehtijev <123364630+rasim-mehtijev@users.noreply.github.com>
2024-09-26 19:15:19 +0300

    bitcoin price, currency symbol (#10)

--- src/Body/PriceNumber.jsx
+++ src/Body/PriceNumber.jsx
@@ -1,8 +1,13 @@
 import { NumericFormat } from "react-number-format";
 
-const PriceNumber = ({ value }) => {
+const PriceNumber = ({ value, symbol }) => {
   return (
-    <NumericFormat value={value} thousandSeparator=" " displayType="text" prefix="$"/>
+    <NumericFormat
+      value={value}
+      thousandSeparator=" "
+      displayType="text"
+      prefix={symbol}
+    />
   );
 };
 
