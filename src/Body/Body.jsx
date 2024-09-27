68a55ae1d610fa3f9983cadf995dda21d0adcc74
Rasim <rasim.mehtijev@insly.com>
2024-09-27 21:13:50 +0300

    react router dom, search

--- src/Body/Body.jsx
+++ src/Body/Body.jsx
@@ -1,11 +1,18 @@
+import CoinPage from "./CoinPage";
 import GlobalData from "./GlobalData";
 import ListCoins from "./ListCoins";
+import { Routes, Route } from "react-router-dom";
+import SearchResult from "./SearchResult";
 
 function Body(props) {
   return (
     <>
       <GlobalData />
-      <ListCoins {...props} />
+      <Routes>
+        <Route path="/" element={<ListCoins {...props} />} />
+        <Route path="/coin/:coinId" element={<CoinPage {...props} />} />
+        <Route path="/search/:q" element={<SearchResult />} />
+      </Routes>
     </>
   );
 }
