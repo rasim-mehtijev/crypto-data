9f05a7346411767669cbbc37dd7ab31564be31b1
Rasim Mehtijev <123364630+rasim-mehtijev@users.noreply.github.com>
2024-09-27 21:14:39 +0300

    react router dom, search (#12)

--- src/Body/SearchResult.jsx
+++ src/Body/SearchResult.jsx
@@ -1,9 +1,19 @@
+import React from "react";
 import Table from "react-bootstrap/Table";
+import { useParams } from "react-router-dom";
+import { getSearch } from "../services/api";
 
 function SearchResult() {
+  const { q } = useParams();
+  const [result, setResult] = React.useState({});
+
+  React.useEffect(() => {
+    getSearch(q).then(setResult);
+  }, [q]);
+
   return (
     <>
-      <h3>Search result</h3>
+      <h3>Search result: {q}</h3>
       <Table striped bordered hover>
         <thead>
           <tr>
@@ -12,7 +22,15 @@ function SearchResult() {
             <th>Symbol</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {result.currencies?.map((currency) => (
+            <tr key={currency.id}>
+              <td>{currency.rank}</td>
+              <td>{currency.name}</td>
+              <td>{currency.symbol}</td>
+            </tr>
+          ))}
+        </tbody>
       </Table>
     </>
   );
