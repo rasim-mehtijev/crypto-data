b7805b91cbe88f8344e73c88759261476c2d8cca
Rasim Mehtijev <123364630+rasim-mehtijev@users.noreply.github.com>
2024-09-27 19:44:47 +0300

    Lesson 7 moment, historical data, intervals, charts data (#11)
    * moment, historical data
    
    * intervals, charts data

--- src/Body/CoinPage/CoinChart.jsx
+++ src/Body/CoinPage/CoinChart.jsx
@@ -9,52 +9,7 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
-
-function CoinChart() {
+function CoinChart({ data }) {
   return (
     <ResponsiveContainer width="100%" height={400}>
       <AreaChart
@@ -69,10 +24,10 @@ function CoinChart() {
         }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
+        <XAxis dataKey="timestamp" />
+        <YAxis dataKey="price" />
         <Tooltip />
-        <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+        <Area type="monotone" dataKey="price" stroke="#8884d8" fill="#8884d8" />
       </AreaChart>
     </ResponsiveContainer>
   );
